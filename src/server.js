44ce89183655585259e51e74882e358396fdd474
sidnei <sidneiferreirati@gmail.com>
2024-09-25 04:33:25 -0300

    identificando o method e url

--- src/server.js
+++ src/server.js
@@ -1,7 +1,10 @@
 import http from 'node:http'
 
 const server = http.createServer((request,response)=>{
-    return response.end("Hello world ")
+   const {method,url} = request
+
+    
+    return response.end(`Metodo: ${method} \nURl: ${url}`)
 })
 
 server.listen(3333)
