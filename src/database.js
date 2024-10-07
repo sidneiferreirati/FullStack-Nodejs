18b74e2c71875e95cef8481a6b27e2f224cf2cdf
sidnei <sidneiferreirati@gmail.com>
2024-10-07 20:28:02 -0300

    salvando usuario em memoria

---
+++ src/database.js
@@ -0,0 +1,15 @@
+export class Database {
+  database = {};
+
+  insert(table, data) {
+    if (Array.isArray(this.database[table])) {
+      this.database[table].push(data);
+    } else {
+      this.database[table] = [data];
+    }
+  }
+
+  select(table) {
+    return this.database[table];
+  }
+}
